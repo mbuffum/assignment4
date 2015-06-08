5db2be15d7f29e2d106cd75a8a3e6127e8131614
mbuffum <mbuffum@uvm.edu>
2015-06-08 11:38:50 -0400

    index and extra pics
    I figured I should put SOMETHING on the index page. It's not fantastic,
    but I think it works.

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Assignment 4: Redesigning the Course Style</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >        
+        <meta name="description" content="An index page containing screenshots of our site.">
+        <meta name="author" content="Marina Buffum, Emilie Dzwonar, Ivan Spizizen" >
+    
+        <style> 
+            img {
+                width: 100%
+            }
+             
+            figure{
+                float: left;
+                width: 250px
+            }
+            
+            figcaption {background-color: white;}
+            
+            #pics { background-color: lightblue;}
+            
+            footer{
+                clear: left;
+            }
+        </style>
+    
+    </head>
+    <body>
+        <h1>Assignment 4</h1>
+        <p>We weren't told what to put on our index page, so here are some screenshot previews of our site. <b>Click on the pictures to see them at full resolution!</b></p>
+        <div id='pics'>
+<figure><a href='SyllabusPreview.png'><img src='SyllabusPreview.png'></a><figcaption>Syllabus Page</figcaption></figure>
+<figure><a href='ResizedSyllabus.png'><img src='ResizedSyllabus.png'></a><figcaption>Resized Syllabus Page</figcaption></figure>
+<figure><a href='AdminMenuPreview.png'><img src='AdminMenuPreview.png'></a><figcaption>Admin Menu (at bottom). Also note fixed header.</figcaption></figure>
+<figure><a href='CalendarPreview.png'><img src='CalendarPreview.png'></a><figcaption></figcaption>Calendar Page</figure>
+<figure><a href='ResourcesPreview.png'><img src='ResourcesPreview.png'></a><figcaption></figcaption>Links on Resources Page</figure>
+<figure><a href='PrintPreview.png'><img src='PrintPreview.png'></a><figcaption></figcaption>Print Preview of Syllabus Page</figure>
+        </div>
+<footer>Return to the <a href="../sitemap.php">sitemap</a> for the css files.</footer>
+</body>
+</html>
