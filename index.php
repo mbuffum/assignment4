cbd78c9b8cfbb8b0e58657836584b9132f3f5678
mbuffum <mbuffum@uvm.edu>
2015-06-08 11:42:45 -0400

    Oops!
    minor error fix

--- index.php
+++ index.php
@@ -5,38 +5,38 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >        
         <meta name="description" content="An index page containing screenshots of our site.">
         <meta name="author" content="Marina Buffum, Emilie Dzwonar, Ivan Spizizen" >
-    
+
         <style> 
             img {
                 width: 100%
             }
-             
+
             figure{
                 float: left;
                 width: 250px
             }
-            
+
             figcaption {background-color: white;}
-            
+
             #pics { background-color: lightblue;}
-            
+
             footer{
                 clear: left;
             }
         </style>
-    
+
     </head>
     <body>
         <h1>Assignment 4</h1>
         <p>We weren't told what to put on our index page, so here are some screenshot previews of our site. <b>Click on the pictures to see them at full resolution!</b></p>
         <div id='pics'>
-<figure><a href='SyllabusPreview.png'><img src='SyllabusPreview.png'></a><figcaption>Syllabus Page</figcaption></figure>
-<figure><a href='ResizedSyllabus.png'><img src='ResizedSyllabus.png'></a><figcaption>Resized Syllabus Page</figcaption></figure>
-<figure><a href='AdminMenuPreview.png'><img src='AdminMenuPreview.png'></a><figcaption>Admin Menu (at bottom). Also note fixed header.</figcaption></figure>
-<figure><a href='CalendarPreview.png'><img src='CalendarPreview.png'></a><figcaption></figcaption>Calendar Page</figure>
-<figure><a href='ResourcesPreview.png'><img src='ResourcesPreview.png'></a><figcaption></figcaption>Links on Resources Page</figure>
-<figure><a href='PrintPreview.png'><img src='PrintPreview.png'></a><figcaption></figcaption>Print Preview of Syllabus Page</figure>
+            <figure><a href='SyllabusPreview.png'><img src='SyllabusPreview.png'></a><figcaption>Syllabus Page</figcaption></figure>
+            <figure><a href='ResizedSyllabus.png'><img src='ResizedSyllabus.png'></a><figcaption>Resized Syllabus Page</figcaption></figure>
+            <figure><a href='AdminMenuPreview.png'><img src='AdminMenuPreview.png'></a><figcaption>Admin Menu (at bottom). Also note fixed header.</figcaption></figure>
+            <figure><a href='CalendarPreview.png'><img src='CalendarPreview.png'></a><figcaption>Calendar Page</figcaption></figure>
+            <figure><a href='ResourcesPreview.png'><img src='ResourcesPreview.png'></a><figcaption>Links on Resources Page</figcaption></figure>
+            <figure><a href='PrintPreview.png'><img src='PrintPreview.png'></a><figcaption>Print Preview of Syllabus Page</figcaption></figure>
         </div>
-<footer>Return to the <a href="../sitemap.php">sitemap</a> for the css files.</footer>
-</body>
+        <footer>Return to the <a href="../sitemap.php">sitemap</a> for the css files.</footer>
+    </body>
 </html>
